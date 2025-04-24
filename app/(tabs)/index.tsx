607fdd76f754dde323bb5c54c0b4d9c4bfb1acb9
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2025-04-24 14:46:47 +0800

    Change background color to grey

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import NewsletterCard from '@/components/NewsletterCard';
 import React from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
@@ -6,8 +7,16 @@ export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
       <ScrollView style={styles.scrollView}>
-        {/* News Letter here */}
-        </ScrollView>
+        {Array.from({ length: 100 }).map((_, index) => (
+          <NewsletterCard
+            key={index}
+            title="What is Hyperwood? (Website)"
+            description="Hyperwood is an open-source system for crafting furniture from simple wooden slats that empowers anyone to build robust furniture using minimal tools and locally sourced materials."
+            image="https://picsum.photos/200/300"
+            url="https://picsum.photos/200/300"
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -17,7 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollView: {
-    backgroundColor: 'blue',
+    backgroundColor: 'grey',
   },
   text: {
     fontSize: 42,
